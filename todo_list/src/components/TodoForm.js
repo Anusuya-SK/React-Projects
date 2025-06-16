bfddac8653ac55c40773376900c24916b3d23f6a
Anusuya-SK <anusuyak98.ak@gmail.com>
2025-06-16 11:20:49 +0530

    Todoform-changes

---
+++ todo_list/src/components/TodoForm.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+export const TodoForm = () => {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+        e.prevetDeafault();
+        console.log(value)
+    }
+
+    return (
+        <form className="TodoForm" onSubmit={handleSubmit}>
+            <input className="todo-input" type="text" name="input-text" placeholder="What is the task today?" onChange={(e) => setValue(e.target.value)}></input>
+            <button className="todo-btn" type="submit">Add Task</button>
+        </form>
+    )
+}
