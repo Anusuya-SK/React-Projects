30eefe6bc3efc0c3ad3eaa460d786de7fa4ee4e1
Anusuya-SK <anusuyak98.ak@gmail.com>
2025-06-16 15:17:50 +0530

    todo-list final-commit

--- todo_list/src/components/EditTodoForm.js
+++ todo_list/src/components/EditTodoForm.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const EditTodoForm = () => {
-  return (
-    <div>EditTodoForm</div>
-  )
+export const EditTodoForm = ({task, editTask}) => {
+    const [value, setValue] = useState(task.task);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        value ? editTask(value, task.id) : setValue("");
+        setValue("");
+    }
+
+    return (
+        <form className="TodoForm" onSubmit={handleSubmit}>
+            <input className="todo-input" type="text" name="input-text" placeholder="Update Task" value={value} onChange={(e) => setValue(e.target.value)}></input>
+            <button className={`${value ? "todo-btn" : "todo-btn-disabled"}`} type="submit">Update Task</button>
+        </form>
+    )
 }
--- todo_list/src/components/TodoForm.js
+++ todo_list/src/components/TodoForm.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
     const [value, setValue] = useState("");
 
     const handleSubmit = e => {
-        e.prevetDeafault();
-        console.log(value)
+        e.preventDefault();
+        value ? addTodo(value) : setValue("");
+        setValue("");
     }
 
     return (
         <form className="TodoForm" onSubmit={handleSubmit}>
-            <input className="todo-input" type="text" name="input-text" placeholder="What is the task today?" onChange={(e) => setValue(e.target.value)}></input>
-            <button className="todo-btn" type="submit">Add Task</button>
+            <input className="todo-input" type="text" name="input-text" placeholder="What is the task today?" value={value} onChange={(e) => setValue(e.target.value)}></input>
+            <button className={`${value ? "todo-btn" : "todo-btn-disabled"}`} type="submit">Add Task</button>
         </form>
     )
 }
