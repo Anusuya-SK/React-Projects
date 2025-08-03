287f96c1a7a72b1f87cc59eebe708e2b6e7bc618
Anusuya-SK <anusuyak98.Ak@gmail.com>
2025-08-03 13:53:32 +0530

    Live Search Filter in react

--- react_code_snippets/src/components/SearchFilter.js
+++ react_code_snippets/src/components/SearchFilter.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const data = ['Apple', 'Banana', 'Cherry', 'Grapes', 'Orange', 'Pineapple', 'Strawberry', 'Watermelon'];
 
 function SearchFilter() {
+    const[query, setQuery] = useState("");
+
+    // Filter items based on query (case-insensitive)
+    const filteredlist = data.filter(item => 
+        item.toLowerCase().includes(query.toLowerCase())
+    );
+
   return (
     <div>
+        <h2>Live Search Filter</h2>
+        <input type='text' placeholder='Search a item here...' value={query} onChange={(e) => setQuery(e.target.value)} />
+        {filteredlist.length > 0 ? (
+            <ul>
+            {filteredlist.map((item, index) => <li key={index}>{item}</li> )}
+            </ul>
+        ) : (
+            <p>No items found</p>
+        )}
         <hr />
     </div>
   )
