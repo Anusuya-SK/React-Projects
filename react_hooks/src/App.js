52502cfd2d6ee68860fb62d52d311f09c1cacd5a
Anusuya-SK <anusuyak98.Ak@gmail.com>
2025-08-04 11:50:11 +0530

    React Hooks

---
+++ react_hooks/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UseStateHook from './components/UseStateHook';
+
+function App() {
+  return (
+    <div className="App">
+      <UseStateHook />
+    </div>
+  );
+}
+
+export default App;
