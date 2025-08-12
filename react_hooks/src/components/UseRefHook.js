e01fb9abdea8360551b34342a1c0c7352ff4a27e
Anusuya-SK <anusuyak98.Ak@gmail.com>
2025-08-12 11:12:22 +0530

    useRef Hook

--- react_hooks/src/components/UseRefHook.js
+++ react_hooks/src/components/UseRefHook.js
@@ -1,20 +1,31 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
 function UseRefHook() {
-    const inputEmailRef = useRef(null); // Step 1: Create a ref
+  const [count, setCount] = useState(0);
+  const prevCount = useRef(); // prevCount = { current: undefined }
+  const inputRef = useRef(null);
 
-    const handleClick = () => {
-        // Step 3: Use ref to focus the input
-        inputEmailRef.current.value = 'test';
-    }
+  const focusInput = () => {
+    inputRef.current.focus(); // Direct DOM manipulation
+    inputRef.current.value = "test";
+  };
+
+  // After the first render, it stores the current count (0) in prevCount.current.
+  // This happens after the render. So initially show undefined, then 0, then 1
+  useEffect(() => {
+    prevCount.current = count; // Store current value for next render
+  }, [count]);
 
   return (
     <div>
-        {/* Step 2: Attach ref to input */}
-        <input ref={inputEmailRef} type='email' placeholder='Enter Email'/>
-        <button onClick={handleClick}>Focus Email Field</button>
+      <p>Current: {count}</p>
+      <p>Previous: {prevCount.current}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <hr />
+      <input ref={inputRef} placeholder="Type something..." />
+      <button onClick={focusInput}>Focus Input</button>
     </div>
-  )
+  );
 }
 
 export default UseRefHook
