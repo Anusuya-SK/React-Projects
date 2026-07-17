df0cec2d431b6f7c272e7cd5b569a1b0931d3240
Anusuya-SK <anusuyak98.Ak@gmail.com>
2026-07-17 15:35:38 +0530

    digital clock

--- digital-clock/src/components/DigitalClock.jsx
+++ digital-clock/src/components/DigitalClock.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 function DigitalClock() {
+  const currentTime = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(currentTime);
+
+  const update = () => {
+    const currentTime = new Date().toLocaleTimeString();
+    setTime(currentTime);
+  };
+
+  setInterval(() => {
+    update();
+  }, 1000);
+
   return (
-    <div>
-      <h1>Digital Clock</h1>
+    <div className="container">
+      <h1 className="title">Digital Clock</h1>
+      <p>{time}</p>
     </div>
   );
 }
