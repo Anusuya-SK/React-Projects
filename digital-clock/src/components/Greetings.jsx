98c4588326ee2b29921d6f10565c2db3d7a498d6
Anusuya-SK <anusuyak98.Ak@gmail.com>
2026-07-18 12:40:46 +0530

    greetings and image based on time

--- digital-clock/src/components/Greetings.jsx
+++ digital-clock/src/components/Greetings.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import morning from "../assets/morning.png";
+import cloudy from "../assets/cloudy.png";
+import sun from "../assets/sun.png";
+import moon from "../assets/moon.png";
 
 function Greetings() {
   const date = new Date();
@@ -17,20 +21,26 @@ function Greetings() {
 
   if (hours >= 5 && hours < 12) {
     msg = "Good Morning";
+    image = morning;
   } else if (hours >= 12 && hours < 18) {
     msg = "Good Afternoon";
+    image = sun;
   } else if (hours >= 18 && hours < 22) {
     msg = "Good Evening";
+    image = cloudy;
   } else if (hours >= 22 && hours < 23) {
     msg = "Good Night";
+    image = moon;
   } else {
     msg = "Good Night";
+    image = moon;
   }
 
   return (
-    <div className="container">
+    <div className="container greetings">
       <h1>Greetings Section</h1>
       <h2>{time}</h2>
+      <img src={image} alt="image" />
       <p>{msg}</p>
     </div>
   );
