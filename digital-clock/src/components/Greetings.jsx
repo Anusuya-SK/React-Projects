8938564ef71356d76c589005deb67df7388bc6bd
Anusuya-SK <anusuyak98.Ak@gmail.com>
2026-07-18 12:00:59 +0530

    greetings based on time

--- digital-clock/src/components/Greetings.jsx
+++ digital-clock/src/components/Greetings.jsx
@@ -1,18 +1,37 @@
 import { useState } from "react";
 
 function Greetings() {
-  const currentTime = new Date().toLocaleTimeString();
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString();
   const [time, setTime] = useState(currentTime);
 
   setInterval(() => {
-    const currentTime = new Date().toLocaleTimeString();
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
     setTime(currentTime);
   }, 1000);
+
+  const hours = date.getHours();
+  let msg;
+  let image;
+
+  if (hours >= 5 && hours < 12) {
+    msg = "Good Morning";
+  } else if (hours >= 12 && hours < 18) {
+    msg = "Good Afternoon";
+  } else if (hours >= 18 && hours < 22) {
+    msg = "Good Evening";
+  } else if (hours >= 22 && hours < 23) {
+    msg = "Good Night";
+  } else {
+    msg = "Good Night";
+  }
 
   return (
     <div className="container">
       <h1>Greetings Section</h1>
       <h2>{time}</h2>
+      <p>{msg}</p>
     </div>
   );
 }
